a3f0382d12c568a20776c450b6ab0917a6db3d33
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-02-28 09:55:52 +0200

    convert

--- kori_xml/convert.py
+++ kori_xml/convert.py
@@ -1,8 +1,7 @@
 import xml.etree.ElementTree as ET 
 
-tree = ET.parse('country_data.xml')
+tree = ET.parse('ab.xml')
 root = tree.getroot()
-doc = 'test.csv'
 
 i = tree.iter()
 head = []
@@ -13,14 +12,15 @@ for x in i:
 	data.append(x.attrib)
 	text.append(x.text)
 
-#for x, y, z in zip(head, data, text):
-#    print(x, y, z)
+for x, y, z in zip(head, data, text):
+    print(x, y, z)
 
-for x in data:
-	for y in x:
-		print(y, x[y])
+#for x in data:
+#	for y in x:
+#		print(y, x[y])
 
-
+#for x in text:
+#	print(x)
 
 #for x in root:
 #	print(x.tag, x.attrib)
